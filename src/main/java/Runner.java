642870af91f9ffa9b9040584497644fdcfe4e6fe
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-10 16:07:12 +0100

    find by id method added

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,17 +11,20 @@ public class Runner {
         Folder folder1 = new Folder("Welcome Folder");
         DBHelper.save(folder1);
 
+
         File file1 = new File("Welcome File", 200, ".txt", folder1);
         DBHelper.save(file1);
+
         File file2 = new File("Second File", 100, ".java", folder1);
-        DBHelper.save(file1);
+        DBHelper.save(file2);
+
         File file3 = new File("Third File", 50, ".ruby", folder1);
-        DBHelper.save(file1);
+        DBHelper.save(file3);
 
 
 
 
-        List<File> files = DBHelper.getAll(File.class);
+        List<File> files = DBHelper.getAll(Folder.class);
 
         File foundFile = DBHelper.find(File.class, file2.getId());
 
